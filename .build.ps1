9859d3d739bf8b1faea29916b67aa2f899d30225
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2017-01-27 11:08:54 +0300

    Updated versions

--- .build.ps1
+++ .build.ps1
@@ -117,8 +117,7 @@ task NuGet Package, Version, {
 		<tags>$project_short_name</tags>
 		<dependencies> 
 			<group targetFramework="net45">
-				<dependency id="Netco" version="1.5.5" />
-				<dependency id="ServiceStack.Text" version="4.0.60" />
+				<dependency id="Netco" version="1.5.6" />
 				<dependency id="CuttingEdge.Conditions" version="1.2.0.0" />
 			</group>
 		</dependencies>
