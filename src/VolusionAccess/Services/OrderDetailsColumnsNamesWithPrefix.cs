0e2600462b231684739f9d822186a250f81a51fe
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2018-05-28 16:02:43 +0300

    order details columns set added

---
+++ src/VolusionAccess/Services/OrderDetailsColumnsNamesWithPrefix.cs
@@ -0,0 +1,44 @@
+﻿namespace VolusionAccess.Services
+{
+	public class OrderDetailsColumnsNamesWithPrefix
+	{
+		public static string OrderDetailID = "od.OrderDetailID,";
+		public static string AutoDropShip = "od.AutoDropShip,";
+		public static string CategoryID = "od.CategoryID,";
+		public static string CouponCode = "od.CouponCode,";
+		public static string CustomLineItem = "od.CustomLineItem,";
+		public static string Fixed_ShippingCost = "od.Fixed_ShippingCost,";
+		public static string Fixed_ShippingCost_Outside_LocalRegion = "od.Fixed_ShippingCost_Outside_LocalRegion,";
+		public static string FreeShippingItem = "od.FreeShippingItem,";
+		public static string GiftTrakNumber = "od.GiftTrakNumber,";
+		public static string GiftWrapCost = "od.GiftWrapCost,";
+		public static string IsKitID = "od.IsKitID,";
+		public static string KitID = "od.KitID,";
+		public static string Locked = "od.Locked,";
+		public static string LastModified = "od.LastModified,";
+		public static string OnOrder_Qty = "od.OnOrder_Qty,";
+		public static string OptionID = "od.OptionID,";
+		public static string OptionIDs = "od.OptionIDs,";
+		public static string Package_Type = "od.Package_Type,";
+		public static string Product_Keys_Shipped = "od.Product_Keys_Shipped,";
+		public static string ProductCode = "od.ProductCode,";
+		public static string ProductID = "od.ProductID,";
+		public static string ProductName = "od.ProductName,";
+		public static string ProductPrice = "od.ProductPrice,";
+		public static string QtyOnBackOrder = "od.QtyOnBackOrder,";
+		public static string QtyOnHold = "od.QtyOnHold,";
+		public static string QtyOnPackingSlip = "od.QtyOnPackingSlip,";
+		public static string QtyShipped = "od.QtyShipped,";
+		public static string Quantity = "od.Quantity,";
+		public static string Returned = "od.Returned,";
+		public static string Returned_Date = "od.Returned_Date,";
+		public static string Reward_Points_Given_For_Purchase = "od.Reward_Points_Given_For_Purchase,";
+		public static string ShipDate = "od.ShipDate,";
+		public static string Shipped = "od.Shipped,";
+		public static string Ships_By_Itself = "od.Ships_By_Itself,";
+		public static string TaxableProduct = "od.TaxableProduct,";
+		public static string TotalPrice = "od.TotalPrice,";
+		public static string Vendor_Price = "od.Vendor_Price,";
+		public static string Warehouses = "od.Warehouses";
+	}
+}
---
+++ src/VolusionAccess/Services/OrderDetailsColumnsSets.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using Netco.Extensions;
+
+namespace VolusionAccess.Services
+{
+	public class OrderDetailsColumnsSets
+	{
+		public static HashSet< string > AllColumnsSet
+		{
+			get { return _allColumnsSet.ToHashSet(); }
+		}
+
+		public static HashSet< string > MinimalColumnsSet
+		{
+			get { return _minimalColumnsSet.ToHashSet(); }
+		}
+
+		private static readonly HashSet< string > _allColumnsSet = new HashSet< string >()
+		{
+			OrderDetailsColumnsNamesWithPrefix.OrderDetailID,
+			OrderDetailsColumnsNamesWithPrefix.AutoDropShip,
+			OrderDetailsColumnsNamesWithPrefix.CategoryID,
+			OrderDetailsColumnsNamesWithPrefix.CouponCode,
+			OrderDetailsColumnsNamesWithPrefix.CustomLineItem,
+			OrderDetailsColumnsNamesWithPrefix.Fixed_ShippingCost,
+			OrderDetailsColumnsNamesWithPrefix.Fixed_ShippingCost_Outside_LocalRegion,
+			OrderDetailsColumnsNamesWithPrefix.FreeShippingItem,
+			OrderDetailsColumnsNamesWithPrefix.GiftTrakNumber,
+			OrderDetailsColumnsNamesWithPrefix.GiftWrapCost,
+			OrderDetailsColumnsNamesWithPrefix.IsKitID,
+			OrderDetailsColumnsNamesWithPrefix.KitID,
+			OrderDetailsColumnsNamesWithPrefix.Locked,
+			OrderDetailsColumnsNamesWithPrefix.LastModified,
+			OrderDetailsColumnsNamesWithPrefix.OnOrder_Qty,
+			OrderDetailsColumnsNamesWithPrefix.OptionID,
+			OrderDetailsColumnsNamesWithPrefix.OptionIDs,
+			OrderDetailsColumnsNamesWithPrefix.Package_Type,
+			OrderDetailsColumnsNamesWithPrefix.Product_Keys_Shipped,
+			OrderDetailsColumnsNamesWithPrefix.ProductCode,
+			OrderDetailsColumnsNamesWithPrefix.ProductID,
+			OrderDetailsColumnsNamesWithPrefix.ProductName,
+			OrderDetailsColumnsNamesWithPrefix.ProductPrice,
+			OrderDetailsColumnsNamesWithPrefix.QtyOnBackOrder,
+			OrderDetailsColumnsNamesWithPrefix.QtyOnHold,
+			OrderDetailsColumnsNamesWithPrefix.QtyOnPackingSlip,
+			OrderDetailsColumnsNamesWithPrefix.QtyShipped,
+			OrderDetailsColumnsNamesWithPrefix.Quantity,
+			OrderDetailsColumnsNamesWithPrefix.Returned,
+			OrderDetailsColumnsNamesWithPrefix.Returned_Date,
+			OrderDetailsColumnsNamesWithPrefix.Reward_Points_Given_For_Purchase,
+			OrderDetailsColumnsNamesWithPrefix.ShipDate,
+			OrderDetailsColumnsNamesWithPrefix.Shipped,
+			OrderDetailsColumnsNamesWithPrefix.Ships_By_Itself,
+			OrderDetailsColumnsNamesWithPrefix.TaxableProduct,
+			OrderDetailsColumnsNamesWithPrefix.TotalPrice,
+			OrderDetailsColumnsNamesWithPrefix.Vendor_Price,
+			OrderDetailsColumnsNamesWithPrefix.Warehouses,
+		};
+
+		private static readonly HashSet< string > _minimalColumnsSet = new HashSet< string >()
+		{
+			OrderDetailsColumnsNamesWithPrefix.OrderDetailID,
+			//OrderDetailsColumnsNamesWithPrefix.AutoDropShip,
+			//OrderDetailsColumnsNamesWithPrefix.CategoryID,
+			//OrderDetailsColumnsNamesWithPrefix.CouponCode,
+			//OrderDetailsColumnsNamesWithPrefix.CustomLineItem,
+			//OrderDetailsColumnsNamesWithPrefix.Fixed_ShippingCost,
+			//OrderDetailsColumnsNamesWithPrefix.Fixed_ShippingCost_Outside_LocalRegion,
+			//OrderDetailsColumnsNamesWithPrefix.FreeShippingItem,
+			//OrderDetailsColumnsNamesWithPrefix.GiftTrakNumber,
+			//OrderDetailsColumnsNamesWithPrefix.GiftWrapCost,
+			//OrderDetailsColumnsNamesWithPrefix.IsKitID,
+			//OrderDetailsColumnsNamesWithPrefix.KitID,
+			//OrderDetailsColumnsNamesWithPrefix.Locked,
+			OrderDetailsColumnsNamesWithPrefix.LastModified,
+			//OrderDetailsColumnsNamesWithPrefix.OnOrder_Qty,
+			//OrderDetailsColumnsNamesWithPrefix.OptionID,
+			//OrderDetailsColumnsNamesWithPrefix.OptionIDs,
+			//OrderDetailsColumnsNamesWithPrefix.Package_Type,
+			//OrderDetailsColumnsNamesWithPrefix.Product_Keys_Shipped,
+			OrderDetailsColumnsNamesWithPrefix.ProductCode,
+			//OrderDetailsColumnsNamesWithPrefix.ProductID,
+			//OrderDetailsColumnsNamesWithPrefix.ProductName,
+			OrderDetailsColumnsNamesWithPrefix.ProductPrice,
+			//OrderDetailsColumnsNamesWithPrefix.QtyOnBackOrder,
+			//OrderDetailsColumnsNamesWithPrefix.QtyOnHold,
+			//OrderDetailsColumnsNamesWithPrefix.QtyOnPackingSlip,
+			//OrderDetailsColumnsNamesWithPrefix.QtyShipped,
+			OrderDetailsColumnsNamesWithPrefix.Quantity,
+			//OrderDetailsColumnsNamesWithPrefix.Returned,
+			OrderDetailsColumnsNamesWithPrefix.Returned_Date,
+			//OrderDetailsColumnsNamesWithPrefix.Reward_Points_Given_For_Purchase,
+			OrderDetailsColumnsNamesWithPrefix.ShipDate,
+			OrderDetailsColumnsNamesWithPrefix.Shipped,
+			OrderDetailsColumnsNamesWithPrefix.Ships_By_Itself,
+			//OrderDetailsColumnsNamesWithPrefix.TaxableProduct,
+			OrderDetailsColumnsNamesWithPrefix.TotalPrice,
+			OrderDetailsColumnsNamesWithPrefix.Vendor_Price,
+			//OrderDetailsColumnsNamesWithPrefix.Warehouses
+		};
+	}
+}
