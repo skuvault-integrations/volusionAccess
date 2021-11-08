c4bec9d4732cfb726a8c85dd1aa8fc079c560dec
jlopezskuv <87334863+jlopezskuv@users.noreply.github.com>
2021-11-08 14:55:21 -0600

    GUARD-2232 Implement Throttler class
    * Limit the inventory update to 100

--- src/VolusionAccess/Misc/ActionPolicies.cs
+++ src/VolusionAccess/Misc/ActionPolicies.cs
@@ -7,12 +7,17 @@ namespace VolusionAccess.Misc
 {
 	public static class ActionPolicies
 	{
+#if DEBUG
+		private const int RetryCount = 1;
+#else
+		private const int RetryCount = 10;
+#endif
 		public static ActionPolicy Submit
 		{
 			get { return _volusionSumbitPolicy; }
 		}
 
-		private static readonly ActionPolicy _volusionSumbitPolicy = ActionPolicy.Handle< Exception >().Retry( 10, ( ex, i ) =>
+		private static readonly ActionPolicy _volusionSumbitPolicy = ActionPolicy.Handle< Exception >().Retry( RetryCount, ( ex, i ) =>
 		{
 			VolusionLogger.Log.Trace( ex, "Retrying Volusion API submit call for the {0} time", i );
 			SystemUtil.Sleep( TimeSpan.FromSeconds( 0.5 + i ) );
@@ -23,7 +28,7 @@ namespace VolusionAccess.Misc
 			get { return _volusionSumbitAsyncPolicy; }
 		}
 
-		private static readonly ActionPolicyAsync _volusionSumbitAsyncPolicy = ActionPolicyAsync.Handle< Exception >().RetryAsync( 10, async ( ex, i ) =>
+		private static readonly ActionPolicyAsync _volusionSumbitAsyncPolicy = ActionPolicyAsync.Handle< Exception >().RetryAsync( RetryCount, async ( ex, i ) =>
 		{
 			VolusionLogger.Log.Trace( ex, "Retrying Volusion API submit call for the {0} time", i );
 			await Task.Delay( TimeSpan.FromSeconds( 0.5 + i ) );
@@ -34,7 +39,7 @@ namespace VolusionAccess.Misc
 			get { return _volusionGetPolicy; }
 		}
 
-		private static readonly ActionPolicy _volusionGetPolicy = ActionPolicy.Handle< Exception >().Retry( 10, ( ex, i ) =>
+		private static readonly ActionPolicy _volusionGetPolicy = ActionPolicy.Handle< Exception >().Retry( RetryCount, ( ex, i ) =>
 		{
 			VolusionLogger.Log.Trace( ex, "Retrying Volusion API get call for the {0} time", i );
 			SystemUtil.Sleep( TimeSpan.FromSeconds( 0.5 + i ) );
@@ -45,7 +50,7 @@ namespace VolusionAccess.Misc
 			get { return _volusionGetAsyncPolicy; }
 		}
 
-		private static readonly ActionPolicyAsync _volusionGetAsyncPolicy = ActionPolicyAsync.Handle< Exception >().RetryAsync( 10, async ( ex, i ) =>
+		private static readonly ActionPolicyAsync _volusionGetAsyncPolicy = ActionPolicyAsync.Handle< Exception >().RetryAsync( RetryCount, async ( ex, i ) =>
 		{
 			VolusionLogger.Log.Trace( ex, "Retrying Volusion API get call for the {0} time", i );
 			await Task.Delay( TimeSpan.FromSeconds( 0.5 + i ) );
---
+++ src/VolusionAccess/Misc/VolusionThrottler.cs
@@ -0,0 +1,48 @@
+﻿using Netco.ThrottlerServices;
+
+namespace VolusionAccess.Misc
+{
+    public class VolusionThrottler : Throttler
+    {
+        /// <summary>
+        /// Throttler constructor. See code section for details
+        /// </summary>
+        /// <code>
+        /// // Maximum request quota: 20 requests. Restore rate: Five items every second
+        /// var throttler = new Throttler( 20, 1, 5 )
+        /// 
+        /// // Maximum request quota of six and a restore rate of one request every minute
+        /// var throttler = new Throttler( 6, 60, 1 )
+        /// </code>
+        /// <param name="maxQuota">Max quota</param>
+        /// <param name="delayInSecondsBeforeRelease">Delay in seconds before release</param>
+        /// <param name="itemsCountForRelease">Items count for release. Default is 1</param>
+        /// <param name="maxRetryCount">Max Retry Count</param>
+        public VolusionThrottler( int maxQuota = 40, int delayInSecondsBeforeRelease = 1, int itemsCountForRelease = 2, int maxRetryCount = 30 ):
+            base( maxQuota, delayInSecondsBeforeRelease, itemsCountForRelease, maxRetryCount, "Too Many Requests" )
+        {
+        }
+    }
+    
+    public class VolusionThrottlerAsync: ThrottlerAsync
+    {
+        /// <summary>
+        /// Throttler constructor. See code section for details
+        /// </summary>
+        /// <code>
+        /// // Maximum request quota: 20 requests. Restore rate: Five items every second
+        /// var throttler = new Throttler( 20, 1, 5 )
+        /// 
+        /// // Maximum request quota of six and a restore rate of one request every minute
+        /// var throttler = new Throttler( 6, 60, 1 )
+        /// </code>
+        /// <param name="maxQuota">Max quota</param>
+        /// <param name="delayInSecondsBeforeRelease">Delay in seconds before release</param>
+        /// <param name="itemsCountForRelease">Items count for release. Default is 1</param>
+        /// <param name="maxRetryCount">Max Retry Count</param>
+        public VolusionThrottlerAsync( int maxQuota = 40, int delayInSecondsBeforeRelease = 1, int itemsCountForRelease = 2, int maxRetryCount = 30 ):
+            base( maxQuota, delayInSecondsBeforeRelease, itemsCountForRelease, maxRetryCount, "Too Many Requests" )
+        {
+        }
+    }
+}
