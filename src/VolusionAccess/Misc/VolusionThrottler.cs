4f4a6989a1babd9b771d00f975fcf301a7758631
jlopezskuv <87334863+jlopezskuv@users.noreply.github.com>
2021-11-10 09:59:04 -0600

    GUARD-2232 Update VolusionThrottler.cs values

--- src/VolusionAccess/Misc/VolusionThrottler.cs
+++ src/VolusionAccess/Misc/VolusionThrottler.cs
@@ -18,8 +18,8 @@ namespace VolusionAccess.Misc
         /// <param name="delayInSecondsBeforeRelease">Delay in seconds before release</param>
         /// <param name="itemsCountForRelease">Items count for release. Default is 1</param>
         /// <param name="maxRetryCount">Max Retry Count</param>
-        public VolusionThrottler( int maxQuota = 1, int delayInSecondsBeforeRelease = 1, int itemsCountForRelease = 1, int maxRetryCount = 10 ):
-            base( maxQuota, delayInSecondsBeforeRelease, itemsCountForRelease, maxRetryCount, "" )
+        public VolusionThrottler( int maxQuota = 1, int delayInSecondsBeforeRelease = 1, int itemsCountForRelease = 1, int maxRetryCount = 1 ):
+            base( maxQuota, delayInSecondsBeforeRelease, itemsCountForRelease, maxRetryCount, "throttle" )
         {
         }
     }
@@ -40,8 +40,8 @@ namespace VolusionAccess.Misc
         /// <param name="delayInSecondsBeforeRelease">Delay in seconds before release</param>
         /// <param name="itemsCountForRelease">Items count for release. Default is 1</param>
         /// <param name="maxRetryCount">Max Retry Count</param>
-        public VolusionThrottlerAsync( int maxQuota = 1, int delayInSecondsBeforeRelease = 1, int itemsCountForRelease = 1, int maxRetryCount = 10 ):
-            base( maxQuota, delayInSecondsBeforeRelease, itemsCountForRelease, maxRetryCount, "" )
+        public VolusionThrottlerAsync( int maxQuota = 1, int delayInSecondsBeforeRelease = 1, int itemsCountForRelease = 1, int maxRetryCount = 1 ):
+            base( maxQuota, delayInSecondsBeforeRelease, itemsCountForRelease, maxRetryCount, "throttle" )
         {
         }
     }
